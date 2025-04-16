6ad04604fdf40a6f95ac70debe4292f78f0735f7
Rémy JARDIN <63077361+RemyJrd@users.noreply.github.com>
2025-04-15 21:49:15 -0400

    Time: 0 ms (100%), Space: 17.8 MB (51.79%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        alphabet = set("abcdefghijklmnopqrstuvwxyz")
+
+        for char in sentence:
+            if char in alphabet:
+                alphabet.remove(char)
+        
+        return len(alphabet) == 0
